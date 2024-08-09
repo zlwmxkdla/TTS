c74f0331d3e52d99fc6e6e4d6391537de25b073d
zlwmxkdla <126744281+zlwmxkdla@users.noreply.github.com>
2024-08-09 20:14:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ import setuptools.command.develop
 from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
 
-if LooseVersion(sys.version) < LooseVersion("3.6") or LooseVersion(sys.version) > LooseVersion("3.9"):
+if LooseVersion(sys.version) < LooseVersion("3.6") or LooseVersion(sys.version) > LooseVersion("3.13"):
     raise RuntimeError(
-        "TTS requires python >= 3.6 and <3.9 "
+        "TTS requires python >= 3.6 and <3.13 "
         "but your Python version is {}".format(sys.version)
     )
 
